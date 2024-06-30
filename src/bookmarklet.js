987ed969daa2ea5070d1fff83780cdeb40e689d5
tyeth <tyethgundry@googlemail.com>
2024-06-30 20:33:01 +0100

    Update jsdelivr links to v0.0.105

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -9,7 +9,7 @@
 
     // Create a style element for CSS
     var style = document.createElement("style");
-    style.textContent = "@import url('https://cdn.jsdelivr.net/gh/tyeth/serialfruit-connect@v0.0.104/src/style.css');";
+    style.textContent = "@import url('https://cdn.jsdelivr.net/gh/tyeth/serialfruit-connect@v0.0.105/src/style.css');";
     style.setAttributeNode(document.createAttribute("scoped"));
     let crs = document.createAttribute("crossorigin");
     crs.value = "anonymous";
@@ -32,7 +32,7 @@
         console.log('Panel created');
         
         // Add the content from your index.html to the panel
-        fetch("https://cdn.jsdelivr.net/gh/tyeth/serialfruit-connect@v0.0.104/src/index.html")
+        fetch("https://cdn.jsdelivr.net/gh/tyeth/serialfruit-connect@v0.0.105/src/index.html")
             .then(response => response.text())
             .then(html => {
                 panel.innerHTML = html + panel.innerHTML;
@@ -65,6 +65,6 @@
         debugger;
         console.error('Failed to load the script', error);
     };
-    script.src = "https://cdn.jsdelivr.net/gh/tyeth/serialfruit-connect@v0.0.104/src/script.js";
+    script.src = "https://cdn.jsdelivr.net/gh/tyeth/serialfruit-connect@v0.0.105/src/script.js";
     document.body.appendChild(script);
 })();
